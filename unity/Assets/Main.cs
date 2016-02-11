a0e9786fc6b3ba2b6c49e6ae5c77fdc9a45f745b
Łukasz Ciupa <losogitos@gmail.com>
2016-02-11 14:00:49 +0100

    Dinosaur axis switch visible glitch fixed.

--- unity/Assets/Main.cs
+++ unity/Assets/Main.cs
@@ -9,8 +9,17 @@ public class Main : MonoBehaviour {
     public GameObject cat;
 
     void Start() {
-        // there is need for cat to be active at beginning, so we have to deactive it here. If cat is not active
-        // at beginning it causes strange problem with not being able to activate it.
+        /**
+         * there is a need for cat to be active at beginning, so we have to deactive it here. If cat is not active
+         * at beginning it causes strange problem with not being able to activate it.
+         * A dinousaur stays active till one of activate functions is executed. It's because the dinosaur has got z axis
+         * at minus value and it implicates that there is a little switch visible at the moment of activation but only if it has
+         * not been activated before. After first activation everything is fine. So in practice the best choice was to leave
+         * it activated and give Unity time to make z axis minus value. At the time when one of the activation functions
+         * below is executed the dinosaur has got already z axis reverted.
+         * The dinousaur would be with z axis with positive value but then it is turned to the user by its worse side (less
+         * effective one). The only way is to negate the z or x axis. So that's why all of this.
+         */
         cat.SetActive(false);
 
 //        logo.SetActive(true);
